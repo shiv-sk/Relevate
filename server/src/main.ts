fe4bf159a09f93c4ff4e76d4fd047f23a2e73aea
shivanand-k <shivanandcrew034@gmail.com>
2025-12-31 20:40:34 +0530

    changed some config

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,8 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://relevate.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
     preflightContinue: false,
     optionsSuccessStatus: 200,
@@ -16,4 +17,5 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.PORT ?? 3001);
 }
+
 bootstrap();
